6399ed181db5cf81c9d1b80ac6e255af7665de39
fazlullahb0 <fazlullahb0@gmail.com>
2022-07-06 20:39:55 +0530

    python

---
+++ Python/one.py
@@ -0,0 +1,13 @@
+#one.py
+
+print("One.py")
+
+def func():
+    print("func() in one.py ")
+
+print("Top level in one.py")
+
+if __name__ == "__main__":
+    print("one.py is being run directly!")
+else:
+    print("one.py has been imported!")
