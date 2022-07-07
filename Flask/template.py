9f068c43cc4109e80342d8fe9c42ac51a1a0bbe5
fazlullahb0 <fazlullahb0@gmail.com>
2022-07-07 12:33:24 +0530

    flask

--- Flask/template.py
+++ Flask/template.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    user_logged_in = True
     company = "Dell"
     letter = list(company)
-    return render_template('basic.html',variable =company, letters = letter)
+    mylist = [10,20,30,40,50,60,72,80,90,100]
+    return render_template('basic.html',variable =company, letters = letter,a_list = mylist, login=user_logged_in)
 
 
 if __name__ == '__main__':
