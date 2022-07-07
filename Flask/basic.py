8b9846bfed0d675bce2671a1f973f4708f007f7e
fazlullahb0 <fazlullahb0@gmail.com>
2022-07-07 10:56:53 +0530

    flask

--- Flask/basic.py
+++ Flask/basic.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello Puppy </h1>"
 
+@app.route('/info')
+def info():
+    return "<h1> Puppies are cuete </h1>"
+
+@app.route('/<name>')
+def puppy(name):
+    return "<h1> This is a page for {} </h1>".format(name)
+
 if __name__ == '__main__':
     app.run(debug=True)
