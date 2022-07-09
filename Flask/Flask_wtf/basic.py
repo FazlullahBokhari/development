fbfd097e7d01bf853c5c89d2daa0a65ffd39e1f9
fazlullahb0 <fazlullahb0@gmail.com>
2022-07-09 14:20:14 +0530

    flash

--- Flask/Flask_wtf/basic.py
+++ Flask/Flask_wtf/basic.py
@@ -20,7 +20,7 @@ def index():
     if form.validate_on_submit():
         breed = form.breed.data
         form.breed.data = ""
-    return render_template('index.html',form=form,breed=breed)
+    return render_template('basic.html',form=form,breed=breed)
 
 if __name__ == "__main__":
     app.run(debug=True)
