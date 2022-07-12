d5e0c177845b533308e3398b733b62d0d735fbd2
fazlullahb0 <fazlullahb0@gmail.com>
2022-07-12 09:57:10 +0530

    db

---
+++ DB_task/form.py
@@ -0,0 +1,18 @@
+#form.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField,SubmitField
+
+class AddForm(FlaskForm):
+
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Add Puppy')
+
+class DelForm(FlaskForm):
+
+    id = IntegerField("Id Number of Puppy to Remove: ")
+    submit = SubmitField('Remove Puppy')
+
+class AddOwnerForm(FlaskForm):
+    name = StringField('Name of Owner: ')
+    puppy_id = IntegerField("Puppy ID : ")
+    submit = SubmitField('Add Owner')
