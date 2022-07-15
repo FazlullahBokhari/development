360d0ec0d951da86c74fc65fba6c24daa5d2fdd3
fazlullahb0 <fazlullahb0@gmail.com>
2022-07-15 10:14:26 +0530

    db

--- User_Authentication/user_auth/myproject/__init__.py
+++ User_Authentication/user_auth/myproject/__init__.py
@@ -13,11 +13,11 @@ app.config['SECRET_KEY'] = 'APP'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app,db)
 
 login_manager.init_app(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
